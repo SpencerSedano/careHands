790d87d8927c9eca546e55a54d176206dc62ed6d
SpencerSedano <spencer.sv20@gmail.com>
2025-02-22 14:55:52 +0800

    Implement CORS

--- backend/CareHands/Program.cs
+++ backend/CareHands/Program.cs
@@ -15,7 +15,21 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(opt => 
   opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5174");
+            policy.AllowAnyHeader();
+            policy.AllowAnyMethod();
+            policy.AllowCredentials();
+        });
+});
+
 var app = builder.Build();
+
+
 
 app.MapIdentityApi<IdentityUser>();
 
@@ -23,6 +37,8 @@ app.MapOpenApi();
 app.MapControllers();
 
 app.MapControllers();
+
+app.UseCors();
 
 // app.UseExceptionHandler("/error");
 
